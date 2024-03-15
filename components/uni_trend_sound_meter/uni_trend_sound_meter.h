08dfaff071ab36e3714cd7d89dd375f012ecf4f5
andyHA <adizanni@msn.com>
2024-03-15 16:55:27 +0100

    Update uni_trend_sound_meter.h

--- components/uni_trend_sound_meter/uni_trend_sound_meter.h
+++ components/uni_trend_sound_meter/uni_trend_sound_meter.h
@@ -17,7 +17,7 @@ using namespace ble_client;
 
 espbt::ESPBTUUID uuid128_from_string(std::string value);
 
-class UnitTrendSoundMeter : public sensor::Sensor, public PollingComponent, public BLEClientBase, public BLEClientNode {
+class UnitTrendSoundMeter : public sensor::Sensor, public PollingComponent, public BLEClientNode {
 
 public:
   void dump_config() override;
